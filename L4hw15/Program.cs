d74438842399ee3030ddcb9323d9c108cc9001a3
P.S.Pokalnetov <redx@mail.ru>
2022-10-17 17:43:42 +0500

    + add L4hw15 (Homework 15)

---
+++ L4hw15/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        int number;
+        Console.Write("Введите число: ");
+        try
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((number > 0 & number < 8) ? "да" : "нет");
+            /* или тоже самое в записи if-else
+                if (number>0 & number<8){
+                    Console.WriteLine("да");    
+                } else {
+                    Console.WriteLine("нет");
+                }
+            */
+        }
+        catch
+        {
+            Console.WriteLine("введено не число,или что-то пошло не так");
+        }
+    }
+}
