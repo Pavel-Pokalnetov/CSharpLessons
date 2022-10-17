e10f384e9937bb9899275dc7c6f5c0ac94dd1d0a
P.S.Pokalnetov <redx@mail.ru>
2022-10-17 16:28:23 +0500

    + add L4hw10 (Homework 10)

---
+++ L4hw10/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+	private static bool IsInt32(string checkSymbol)
+	{
+		try
+		{
+			int temp = Convert.ToInt32(checkSymbol);
+			return true;
+		}
+		catch
+		{
+			return false;
+		}
+	}
+	private static void Main()
+	{
+		Console.Write("Введите 3х-значное число: ");
+		string number = Console.ReadLine();
+		if (IsInt32(number)){
+			if (number.Length == 3){
+				Console.WriteLine(number[1]);
+				return;}
+			Console.WriteLine("число не трехзначное");
+			return;
+		}
+		Console.WriteLine("введено не число");
+		
+		
+		
+	}
+}
