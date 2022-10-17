edba309eb1aef979b71e5e162921668095ad744d
P.S.Pokalnetov <pokalnetovps@gmail.com>
2022-10-18 01:09:21 +0500

    +final fix homework 10,13,15

--- L4hw10/Program.cs
+++ L4hw10/Program.cs
@@ -2,8 +2,20 @@
 {
 	private static void Main()
 	{
-		int rndInt = new Random().Next(100, 1000);
-		int result = (rndInt % 100) / 10;
-		Console.WriteLine($"{rndInt} , {result}");
+		int cnsEnter = 0;
+		try
+		{   //проверка на правильный ввод через исключения
+			Console.Write("Введите число: ");
+			cnsEnter = int.Parse(Console.ReadLine());
+			if (cnsEnter < 100 || cnsEnter > 999) throw new Exception();
+		}
+		catch
+		{
+			Console.WriteLine($"ввдена не цифра или введено число не из диапазона 100...999");
+			return;
+		}
+
+		//	int result = (cnsEnter % 100) / 10;
+		Console.WriteLine($"исходное число: {cnsEnter}, средняя(2я) цифра: {(cnsEnter % 100) / 10}");
 	}
 }
