25d39058fcf7689ecb3c1ad3023dd1f0e72bc8bb
P.S.Pokalnetov <redx@mail.ru>
2022-10-17 17:25:14 +0500

    + add L4hw13 (Homework 13)

---
+++ L4hw13/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static bool IsInt32(string check)// return True if check is int32 number
+    {
+        try
+        {
+            int temp = Convert.ToInt32(check);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    private static void Main()
+    {
+        Console.Write("Введите число: ");
+        string number = Console.ReadLine();
+        if (IsInt32(number))
+        {
+            if (number.Length > 2)
+            {
+                Console.WriteLine(number[2]);
+            }
+            else
+            {
+                Console.WriteLine("третьей цифры нет");
+            }
+        }
+        else
+        {
+            Console.WriteLine("введено не число");
+        }
+
+    }
+}
