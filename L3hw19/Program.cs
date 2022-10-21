e01a21bb7a6d0c7282044df132d6f5642f6530fe
P.S.Pokalnetov <pokalnetovps@gmail.com>
2022-10-21 19:42:14 +0500

    fix hw19

--- L3hw19/Program.cs
+++ L3hw19/Program.cs
@@ -2,9 +2,8 @@
 {
     private static int getNumInPos(int num, int pos) //возвращает цифру в определенной позиции ( нумерация с 0)
     {
-        int numLen = count10(num) - 1;
-        int currentPos = numLen;
-        if (numLen < pos)
+        int currentPos = 4;
+        if (pos > 4)
         {
             throw new Exception("index out of range");
         }
@@ -16,29 +15,25 @@
         num %= 10;
         return num;
     }
-    private static int count10(int x)//число цифр в числе
-    {
-        int t = 1;
-        while (x > 10)
-        {
-            x = x / 10;
-            t++;
-        }
-        return t;
-    }
 
     private static string checkPolindrom(int num)//проверка на палиндром (да/нет)
     {
-        if (num < 0) return "нет";
-        int numLen = count10(num);
-        for (int i = 0; i < numLen; i++) if (getNumInPos(num, i) != getNumInPos(num, numLen - 1 - i)) return "нет";
+        if (num < 10000 || num > 99999)
+        {
+            Console.WriteLine("число вне диапазона 10000..99999");
+            Environment.Exit(1); //Выход с кодом 1
+        }
+        for (int i = 0; i < 5; i++) if (getNumInPos(num, i) != getNumInPos(num, 4 - i)) return "нет";
         return "да";
     }
 
     private static void Main(string[] args)
     {
-        int number;Console.Write("Введите число (0 для выхода): ");
+        for(int i = 0; i < 3; i++) { 
+        int number; Console.Write("Введите пятизначное число: ");
         number = int.Parse(Console.ReadLine());
+
         Console.WriteLine($"{number} -> {checkPolindrom(number)}");
+    }
     }
 }
