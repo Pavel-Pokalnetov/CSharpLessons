918f883b39b653c7b92948cd66cf527d4f9051a3
Pokalnetov Pavel Sergeevich <pokalnetovps@gmail.com>
2022-11-12 15:23:23 +0500

    задача 34 готова

--- L5hw34/Program.cs
+++ L5hw34/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+ */
+
+using System.Threading.Tasks.Dataflow;
+
+internal class Program
+{
+    private static String ArrayToString(int[] array)
+    {
+        return "["+String.Join(", ", array)+"]";
+    }
+    
+    private static int CheckParityCount(int[] array)
+    {
+        int count = 0;
+        foreach (int elenentOfArray in array)
+        {
+            count += elenentOfArray % 2 == 0 ? 1 : 0;
+        }
+        return count;
+    }
+
+    private static int[] GetArray(int dim, int min, int max)
+    {
+        Random rnd = new Random();
+        int[] array = new int[dim]; 
+        for(int count = 0; count < dim; count++) { array[count] = rnd.Next(min,max+1);}
+        return array;
+    }
+    
+    private static void Main(string[] args)
+    {
+        int[] myArray = GetArray(4, 100, 999);
+        Console.WriteLine($"{ArrayToString(myArray)} -> {CheckParityCount(myArray)}");
+
+    }
+}
