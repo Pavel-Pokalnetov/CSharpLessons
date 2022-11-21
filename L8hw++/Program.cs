b1f8dc62733045e9fe4de59e44a433ace84db4e5
Покальнетов П.С <pokalnetovps@gmail.com>
2022-11-22 01:26:49 +0500

    задача со звездой метод Герона

---
+++ L8hw++/Program.cs
@@ -0,0 +1,47 @@
+﻿double Abs(double arg)
+{   //модуль числа
+    return arg < 0 ? -arg : arg;
+}
+
+double Round(double arg, int acc)
+{
+    for (int i = 0; i < acc; i++)
+    {
+        arg *= 10;
+    }
+    arg = (int)arg;
+    for (int i = 0; i < acc; i++)
+    {
+        arg /= 10;
+    }
+    return arg;
+}
+
+double funcionHeron(double source, int acc)
+{
+    /* 
+     * Формула Герона https://ru.wikipedia.org/wiki/Итерационная_формула_Герона
+     */
+    double accuracy = 1.0;
+    for (int i = 0; i < acc; i++) { accuracy /= 10.0; }
+    
+    double Xnew = (double)source;
+    double Xold;
+
+    do
+    {
+        Xold = Xnew;
+        Xnew = (Xold + source/Xold)/2;
+    }
+    while (Abs(Xnew - Xold) >= accuracy);
+    return Round(Xnew,acc);
+}
+
+
+
+
+Console.Write("Введите число: ");
+double inputNumber = double.Parse(Console.ReadLine()!);
+Console.Write("Точность (знаков после запятой): ");
+int accuracy = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{funcionHeron(inputNumber,accuracy)}");
